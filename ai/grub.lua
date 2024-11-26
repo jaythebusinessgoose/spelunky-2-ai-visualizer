9c3e68f560729268d7669dfc58c9d848c564b783
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2024-11-25 18:25:20 -0700

    Add Grub turn to fly block boxes.

--- ai/grub.lua
+++ ai/grub.lua
@@ -13,6 +13,28 @@ return Entity_AI:new({
                 return ent.move_state == 0
             end,
             label = "Attack"
+        },
+        { -- Turn to fly (backwall)
+            shape = geometry.create_box_shape(-1, -1, 1, 1),
+            is_visible = function(ent)
+                return ent.move_state == 0 and ent.turn_into_fly_timer ~= -1
+            end,
+            is_active = function(ent)
+                return ent.turn_into_fly_timer == 0
+            end,
+            label = "Turn to fly blockers",
+            label_position = LABEL_POSITION.TOP
+        },
+        { -- Turn to fly (floor)
+            shape = geometry.create_box_shape(-1, 0, 1, 2),
+            is_visible = function(ent)
+                return ent.move_state == 9 and ent.turn_into_fly_timer ~= -1
+            end,
+            is_active = function(ent)
+                return ent.turn_into_fly_timer == 0
+            end,
+            label = "Turn to fly blockers",
+            label_position = LABEL_POSITION.TOP
         }
     }
 })
