8929886e1fa2f1579c233e6dcb55f1eed0d7a7f3
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2024-11-26 00:24:58 -0700

    Merge lavamander's two in-lava aggro ranges into one since it's really just a single thing, and add points for its lava check positions.

--- ai/lavamander.lua
+++ ai/lavamander.lua
@@ -1,3 +1,5 @@
+local HITBOX_H_HALF = get_type(ENT_TYPE.MONS_LAVAMANDER).hitboxx
+
 return Entity_AI:new({
     id = "lavamander",
     name = "Lavamander",
@@ -5,27 +7,16 @@ return Entity_AI:new({
     ranges = {
         { -- Aggro
             -- Once the lavamander sees the player in its aggro range, it will switch to attack mode. It will rise as long as the player is in the attack range, and will attack if it reaches the surface of the lava. It will deaggro after a short time if it isn't able to attack, though it will appear to keep attacking if the player is within the aggro/attack overlap since it immediately reaggros. The first time it surfaces, it will stare at the player for a moment instead of attacking.
-            shape = geometry.create_circle_shape(8),
+            shape = geometry.create_circle_shape(8):clip_box(nil, -1, nil, 4),
             is_visible = function(ent)
                 return ent.is_hot
             end,
             is_active = function(ent)
-                return ent.state == 3 and (ent.move_state == 0 or ent.move_state == 1) and ent.shoot_lava_timer == 0 and ent.chased_target_uid == -1
-            end,
-            label = "Aggro"
-        },
-        { -- Rise and attack
-            shape = geometry.create_box_shape(-1000, -1, 1000, 4),
-            is_visible = function(ent)
-                return ent.is_hot and ent.chased_target_uid >= 0 and ent.shoot_lava_timer == 0
-            end,
-            is_active = function(ent)
-                return (ent.state == 3 or ent.state == CHAR_STATE.FALLING) and (ent.move_state == 0 or ent.move_state == 1)
+                return ent.state == 3 and (ent.move_state == 0 or ent.move_state == 1) and ent.shoot_lava_timer == 0
             end,
             label = function(ent)
                 return ent.player_detect_state == 0 and "Rise and stare" or "Rise and attack"
             end,
-            label_position = LABEL_POSITION.TOP
         },
         { -- Jump
             shape = geometry.create_circle_shape(8):clip_left(0),
@@ -48,6 +39,14 @@ return Entity_AI:new({
                 return ent.state == CHAR_STATE.STANDING and (ent.move_state == 0 or ent.move_state == 1) and ent.jump_pause_timer == 0
             end,
             label = "Turn"
-        }
+        },
+        { -- Lava checks
+            -- Top check is used to stop the lavamander rising during the attack. Both are used to check if the lavamander is in lava and should
+            -- heat/cool.
+            shape = geometry.create_point_set_shape(Vec2:new(0, HITBOX_H_HALF / 2), Vec2:new(0, -HITBOX_H_HALF / 2)),
+            type = Entity_AI.RANGE_TYPE.SOLID_CHECK,
+            label = "Lava Checks",
+            label_position = LABEL_POSITION.BOTTOM
+        },
     }
 })
