57ba1784ff1e808a0118400fcd691613d574463f
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2024-12-09 19:23:36 -0700

    Add rise/lower ranges to UFO.

--- ai/ufo.lua
+++ ai/ufo.lua
@@ -13,6 +13,36 @@ return Entity_AI:new({
                 return ent.move_state == 0 and ent.attack_cooldown_timer == 0
             end,
             label = "Attack"
+        },
+        { -- Rise
+            shape = geometry.create_circle_shape(8):clip_bottom(0),
+            is_visible = function(ent)
+                return ent.move_state ~= 4 and not ent.is_falling
+            end,
+            is_active = function(ent)
+                return ent.move_state == 0
+            end,
+            label = "Rise"
+        },
+        { -- Lower
+            shape = geometry.create_circle_shape(8):clip_top(-4),
+            is_visible = function(ent)
+                return ent.move_state ~= 4 and not ent.is_falling
+            end,
+            is_active = function(ent)
+                return ent.move_state == 0
+            end,
+            label = "Lower"
+        },
+        { -- Stop Rising
+            shape = geometry.create_circle_shape(8):clip_top(-3),
+            is_visible = function(ent)
+                return ent.move_state ~= 4 and ent.is_falling
+            end,
+            is_active = function(ent)
+                return ent.move_state == 0
+            end,
+            label = "Stop Rising"
         }
     }
 })
