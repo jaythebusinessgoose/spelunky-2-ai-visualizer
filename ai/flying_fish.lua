69cb655ec32c07c9dc9e7915ee16c317b039d55b
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2024-12-14 20:13:12 -0700

    Add stop rising range to fish

--- ai/flying_fish.lua
+++ ai/flying_fish.lua
@@ -3,6 +3,12 @@ return Entity_AI:new({
     id = "flying_fish",
     name = "Flying fish",
     ent_type = ENT_TYPE.MONS_FISH,
+    preprocess = function(ent, ctx)
+        ctx.offsetx = ent.offsetx
+        ctx.offsety = ent.offsety
+        ctx.hitboxx = ent.hitboxx
+        ctx.hitboxy = ent.hitboxy
+    end,
     ranges = {
         { -- Rise
             -- TODO: Ceiling will reduce the height of the rise shape. Getting close may cause a rise even when there is a low ceiling.
@@ -19,6 +25,19 @@ return Entity_AI:new({
             end,
             label = "Attack",
             label_position = LABEL_POSITION.TOP
+        },
+        { -- Stop rising
+            shape = function(ent, ctx)
+                local top_pos = ctx.offsety + ctx.hitboxy + 0.15
+                return geometry.create_point_set_shape(Vec2:new(ctx.offsetx - ctx.hitboxx, top_pos), Vec2:new(0, top_pos), Vec2:new(ctx.offsetx + ctx.hitboxx, top_pos))
+            end,
+            type = Entity_AI.RANGE_TYPE.SOLID_CHECK,
+            is_visible = function(ent)
+                return ent.move_state == 2
+            end,
+            label = "Stop Rising",
+            -- TODO: This hack puts the label above the points.
+            label_position = LABEL_POSITION.BOTTOM
         }
     }
 })
