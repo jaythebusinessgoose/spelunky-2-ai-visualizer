10b08441acbebec0ccfa9e6c25651bb6fbd525fc
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2025-05-17 22:11:29 -0600

    Revert giant spider trigger change.

--- ai/giant_spider.lua
+++ ai/giant_spider.lua
@@ -5,14 +5,13 @@ return Entity_AI:new({
     ranges = {
         { -- Aggro
             -- TODO: Width is controlled by trigger_distance field, which defaults to 0.95 for this entity type.
-            shape = geometry.create_circle_shape(7):clip_box(-0.95, nil, 0.95, 0),
+            shape = geometry.create_box_shape(-0.95, -7, 0.95, 0),
             is_visible = function(ent)
                 return ent.state == CHAR_STATE.HANGING
             end,
             label = "Aggro",
         },
         { -- Small Jump
-            -- TODO: Width is controlled by trigger_distance field, which defaults to 0.95 for this entity type.
             shape = geometry.create_box_shape(-1000, -1000, 1000, 0),
             is_visible = function(ent)
                 return ent.state ~= CHAR_STATE.HANGING and ent.move_state == 0
